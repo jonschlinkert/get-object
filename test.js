25dcb911268e8703c1c8d9a88396ccb98f961b95
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-27 04:59:07 -0400

    0.2.0 tests

--- test.js
+++ test.js
@@ -11,13 +11,30 @@ var should = require('should');
 var get = require('./');
 
 describe('.get()', function () {
-  it('should get a value only.', function () {
-    get({a: 'aaa', b: {c: 'd'}}, 'a').should.eql('aaa');
-    get({a: 'a', b: {c: 'd'}}, 'b.c').should.eql('d');
+  it('should get a value from an object.', function () {
+    get({a: 'aaa', b: {c: 'd'}}, 'a').should.eql({a: 'aaa'});
+    get({a: 'a', b: {c: 'd'}}, 'b.c').should.eql({c: 'd'});
   });
 
-  it('should get the value of a deeply nested property.', function () {
-    get({a: {b: 'c', c: {d: 'e', e: 'f', g: {h: 'i'}}}}, 'a.c.g.h').should.eql('i');
+  it('should get a value from an array.', function () {
+    get(['a', 'b', 'c'], '0').should.eql(['a']);
+    get(['a', 'b', 'c'], 2).should.eql(['c']);
+  });
+
+  it('should get nested value from an array.', function () {
+    get({a: ['a', 'b', 'c']}, 'a.0').should.eql(['a']);
+    get({a: ['a', 'b', 'c']}, 'a.2').should.eql(['c']);
+    get({a: {b: ['a', 'b', 'c']}}, 'a.b.2').should.eql(['c']);
+  });
+
+  it('should support array notation.', function () {
+    get({a: ['a', 'b', 'c']}, 'a[0]').should.eql(['a']);
+    get({a: ['a', 'b', 'c']}, 'a[2]').should.eql(['c']);
+    get({a: {b: ['a', 'b', {c: 'd'}]}}, 'a.b[2].c').should.eql({c: 'd'});
+  });
+
+  it('should get a deeply nested property.', function () {
+    get({a: {b: 'c', c: {d: 'e', e: 'f', g: {h: 'i'}}}}, 'a.c.g.h').should.eql({h: 'i'});
   });
 
   it('should return an empty object when the value is undefined.', function () {
@@ -29,9 +46,9 @@ describe('.get()', function () {
       a: {locals : {name: {first: 'Brian'}}},
       b: {locals : {name: {last: 'Woodward'}}}
     };
-    get(fixture, 'a.locals.name').should.eql({first: 'Brian'});
-    get(fixture, 'b.locals.name').should.eql({last: 'Woodward'});
-    get(fixture, 'b.locals.name.last').should.eql('Woodward');
+    get(fixture, 'a.locals.name').should.eql({name: {first: 'Brian'}});
+    get(fixture, 'b.locals.name').should.eql({name: {last: 'Woodward'}});
+    get(fixture, 'b.locals.name.last').should.eql({last: 'Woodward'});
   });
 
   it('should return an empty object if the path is not found', function () {
@@ -41,9 +58,9 @@ describe('.get()', function () {
   });
 
   it('should get the specified property.', function () {
-    get({a: 'aaa', b: 'b'}, 'a').should.eql('aaa');
-    get({first: 'Jon', last: 'Schlinkert'}, 'first').should.eql('Jon');
-    get({locals: {a: 'a'}, options: {b: 'b'}}, 'locals').should.eql({a: 'a'});
+    get({a: 'aaa', b: 'b'}, 'a').should.eql({a: 'aaa'});
+    get({first: 'Jon', last: 'Schlinkert'}, 'first').should.eql({first: 'Jon'});
+    get({locals: {a: 'a'}, options: {b: 'b'}}, 'locals').should.eql({locals: {a: 'a'}});
   });
 
   it('should return the entire object if no property is passed.', function () {
@@ -51,7 +68,7 @@ describe('.get()', function () {
   });
 
   it('should get the value of a deeply nested property.', function () {
-    get({a: {b: 'c', c: {d: 'e', e: 'f', g: {h: 'i'}}}}, 'a.c.g.h').should.eql('i');
+    get({a: {b: 'c', c: {d: 'e', e: 'f', g: {h: 'i'}}}}, 'a.c.g.h').should.eql({h: 'i'});
   });
 
   it('should return an empty object if the first value is null.', function () {
@@ -59,3 +76,20 @@ describe('.get()', function () {
   });
 });
 
+var obj = {
+  prop1: {
+    arr: ['a', 'b', 'c'],
+    str: 'Hello'
+  },
+  prop2: {
+    arr: [{
+      nested: 'Universe'
+    }],
+    str: 'Hello again!'
+  }
+};
+
+
+console.log(get(obj, 'prop1.str'))
+console.log(get(obj, 'prop1.arr[2]'))
+console.log(get(obj, 'prop2.arr[0]nested'))
